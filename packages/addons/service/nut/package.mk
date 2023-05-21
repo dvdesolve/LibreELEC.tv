9ed726970fc80ef5050c8f9a439bb150aababce2
Viktor Drobot <linux776@gmail.com>
2023-05-21 18:17:06 +0300

    Try to implement NUT as an addon

--- packages/addons/service/nut/package.mk
+++ packages/addons/service/nut/package.mk
@@ -3,6 +3,7 @@
 
 PKG_NAME="nut"
 PKG_VERSION="2.8.0"
+PKG_REV="100"
 PKG_SHA256="c3e5a708da797b7c70b653d37b1206a000fcb503b85519fe4cdf6353f792bfe5"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2+"
@@ -12,13 +13,16 @@ PKG_MAINTAINER="Viktor Drobot <linux776@gmail.com> (dvdesolve)"
 PKG_DEPENDS_TARGET="toolchain glibc libtool libusb-compat systemd"
 PKG_LONGDESC="A collection of programs which provide a common interface for monitoring and administering UPS, PDU and SCD hardware"
 
-PKG_TOOLCHAIN="configure"
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Network UPS Tools (NUT)"
+PKG_ADDON_TYPE="xbmc.service"
 
-PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
-                          --datadir=/usr/share/nut \
-                          --libexecdir=/usr/lib/nut \
-                          --sbindir=/usr/bin \
-                          --sysconfdir=/etc/nut \
+PKG_TOOLCHAIN="configure"
+PKG_CONFIGURE_OPTS_TARGET="--prefix=/storage/.kodi/addons/${PKG_ADDON_ID} \
+                          --datadir=/storage/.kodi/addons/${PKG_ADDON_ID}/share \
+                          --libexecdir=/storage/.kodi/addons/${PKG_ADDON_ID}/lib \
+                          --sbindir=/storage/.kodi/addons/${PKG_ADDON_ID}/bin \
+                          --sysconfdir=/storage/.kodi/addons/${PKG_ADDON_ID}/config \
                           --disable-static \
                           --with-user=nut \
                           --with-group=nut \
@@ -26,8 +30,8 @@ PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
                           --with-drvpath=/usr/lib/nut \
                           --with-pidpath=/run/nut \
                           --with-statepath=/var/lib/nut \
-                          --with-systemdsystemunitdir=/usr/lib/systemd/system \
-                          --with-udev-dir=/usr/lib/udev \
+                          --with-systemdsystemunitdir=/storage/.kodi/addons/${PKG_ADDON_ID}/lib/systemd/system \
+                          --with-udev-dir=/storage/.kodi/addons/${PKG_ADDON_ID}/lib/udev \
                           --without-cgi \
                           --without-dev \
                           --without-doc \
@@ -43,9 +47,11 @@ PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
                           --without-powerman \
                           --without-wrap"
 
-post_makeinstall_target() {
-  find ${INSTALL}/storage/nut/etc/nut -name *.sample -exec rename '.sample' '' {} \;
+addon() {
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
+  cp -r ${PKG_INSTALL}/storage/.kodi/addons/${PKG_ADDON_ID}/bin ${PKG_INSTALL}/storage/.kodi/addons/${PKG_ADDON_ID}/lib ${PKG_INSTALL}/storage/.kodi/addons/${PKG_ADDON_ID}/share ${ADDON_BUILD}/${PKG_ADDON_ID}/
+  cp -r ${PKG_INSTALL}/storage/.kodi/addons/${PKG_ADDON_ID}/config ${ADDON_BUILD}/${PKG_ADDON_ID}/
 
-  install -Dm 644 ${PKG_DIR}/nut.sysusers ${INSTALL}/storage/nut/usr/lib/sysusers.d/nut.conf
-  install -Dm 644 ${PKG_DIR}/nut.tmpfiles ${INSTALL}/storage/nut/usr/lib/tmpfiles.d/nut.conf
+  install -Dm 644 ${PKG_DIR}/nut.sysusers ${PKG_INSTALL}/storage/.config/sysusers.d/nut.conf
+  install -Dm 644 ${PKG_DIR}/nut.tmpfiles ${PKG_INSTALL}/storage/.config/tmpfiles.d/nut.conf
 }
